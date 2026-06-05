abbb30b5f0903feb35c8338aac50d55951dc5e08
RidoNanu <ridonanu5105@gmail.com>
2026-06-05 23:11:03 +0530

    Configure Vite and React Router for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout.jsx';
 import Home from './pages/Home.jsx';
 import Products from './pages/Products.jsx';
